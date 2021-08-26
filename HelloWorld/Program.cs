fc9d06ce25ba42a05ddd984a129ce0ecd424e89c
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-08-26 09:53:58 -0500

    chore: update c# version, add a second class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,16 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("HelloWorld!");
+
+            Program2 x = new();
+            x.printStr();
+        }
+    }
+    class Program2
+    {
+        public void printStr()
+        {
+            Console.WriteLine("HelloWorld2!");
         }
     }
 }
