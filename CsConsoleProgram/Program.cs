75c21275555b88ab8d3a021762486fe347539308
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-08-26 15:03:56 -0500

    chore: init new project

---
+++ CsConsoleProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
