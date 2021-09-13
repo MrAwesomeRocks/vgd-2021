b6a1aaac7f0a75d3d679ba93aa9bed6e00c7901b
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-09-12 21:06:21 -0500

    feat: challenge 2

---
+++ CreateWithCode/Prototype-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private const float spawnRate = 2.5f;
+    private float nextSpawn = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
