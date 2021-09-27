a08a0fcbf7448e712f5bf765b016184dc62f6ea9
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-09-27 10:35:45 -0500

    feat: spawn obstacles randomly

--- CreateWithCode/Prototype-3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype-3/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
-    public float startDelay = 2.0f;
-    public float repeatRate = 2.0f;
+    public float startDelay;
+    public float spawnDelayMin;
+    public float spawnDelayMax;
+
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private PlayerController playerControllerScript;
+    [SerializeField] private float spawnDelay;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
+        Invoke(nameof(SpawnObstacle), startDelay);
     }
 
     // Update is called once per frame
@@ -30,5 +33,8 @@ public class SpawnManager : MonoBehaviour
             int index = Random.Range(0, obstaclePrefabs.Length);
             Instantiate(obstaclePrefabs[index], spawnPos, obstaclePrefabs[index].transform.rotation);
         }
+
+        spawnDelay = Random.Range(spawnDelayMin, spawnDelayMax);
+        Invoke(nameof(SpawnObstacle), spawnDelay);
     }
 }
