232a7ef37e3bbf0b1355c2d7ab8f88e7f10e4c34
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-10-08 22:15:38 -0500

    feat: lesson 4.3

--- CreateWithCode/Prototype-4/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype-4/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
 
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    [SerializeField] private float powerupStrength = 15.0f;
+    [SerializeField] private Vector3 powerupOffset = new Vector3(0, -0.5f, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +26,46 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(forwardInput * speed * focalPoint.transform.forward);
+
+        powerupIndicator.transform.position = transform.position + powerupOffset;
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if (hasPowerup && other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(powerupStrength * awayFromPlayer, ForceMode.Impulse);
+            Debug.Log($"Collided with {other.gameObject.name} with powerup set to {hasPowerup}");
+        }
     }
 }
