63b321369d44f875b1b194c0f6260431d4f25ff1
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-10-08 22:29:49 -0500

    feat: lesson 4.4

--- CreateWithCode/Prototype-4/Assets/Scripts/Enemy.cs
+++ CreateWithCode/Prototype-4/Assets/Scripts/Enemy.cs
@@ -22,5 +22,10 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(speed * lookDirection);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- CreateWithCode/Prototype-4/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype-4/Assets/Scripts/SpawnManager.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     [SerializeField] private float spawnRange = 9.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerup();
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
 
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup();
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     private Vector3 GenerateSpawnPosition()
