6f7e73ae970ca67258a41c6160474975db4d11af
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-11-06 18:07:45 -0500

    feat: lesson 6.2

--- CreateWithCode/Prototype-1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype-1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,9 +10,20 @@ public class PlayerController : MonoBehaviour
     public Camera thirdPersonCamera;
     public KeyCode cameraSwitchKey;
 
+    // Driving improvements
+    private Rigidbody playerRb;
+    [SerializeField] private float horsePower;
+    [SerializeField] private GameObject centerOfMass;
+    [SerializeField] List<WheelCollider> allWheels;
+
+    // UI
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] private int speed;
+    [SerializeField] private int rpm;
+
     // control var
-    public int vehicleNumber;
-    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private string inputSuffix;
     [SerializeField] private float turnSpeed = 55.0f;
     [SerializeField] private float horizontalInput = 0.0f;
     [SerializeField] private float forwardInput = 0.0f;
@@ -29,6 +41,9 @@ public class PlayerController : MonoBehaviour
     {
         firstPersonCamera.enabled = false;
         thirdPersonCamera.enabled = true;
+
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     /// <summary>
@@ -36,19 +51,55 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        // get inputs
-        horizontalInput = Input.GetAxis($"Horizontal{vehicleNumber}");
-        forwardInput = Input.GetAxis($"Vertical{vehicleNumber}");
+        if (IsOnGround())
+        {
 
-        // move the vehicle forward based on vertical input
-        transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.forward);
-        // rotate the vehicle forward based on horizontal input
-        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+            // get inputs
+            horizontalInput = Input.GetAxis($"Horizontal{inputSuffix}");
+            forwardInput = Input.GetAxis($"Vertical{inputSuffix}");
+
+            // Drive wheels the right way
+            foreach (WheelCollider wheel in allWheels)
+            {
+                // move the vehicle forward based on vertical input
+                // transform.Translate(forwardInput * speed * Time.deltaTime * Vector3.forward);
+                // playerRb.AddRelativeForce(horsePower * forwardInput * Vector3.forward);
+                wheel.motorTorque = horsePower * forwardInput;
+
+                // rotate the vehicle based on horizontal input
+                //transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+                if (wheel.name.IndexOf("f") != -1)
+                {
+                    // Only front wheels steer
+                    wheel.steerAngle = turnSpeed * horizontalInput;
+                }
+            }
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+            speedometerText.SetText($"Speed: {speed} mph");
+
+            rpm = speed % 30 * 40;
+            rpmText.SetText($"RPM: {rpm}");
+        }
 
         // Cameras
         if (Input.GetKeyDown(cameraSwitchKey))
         {
             ToggleCamera();
         }
+    }
+
+    bool IsOnGround()
+    {
+        int wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        return wheelsOnGround == 4;
     }
 }
