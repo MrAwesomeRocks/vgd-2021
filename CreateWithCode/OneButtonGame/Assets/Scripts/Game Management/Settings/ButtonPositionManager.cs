423247aafc036972169505e8f4295067ef5ba722
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-11-23 09:32:45 -0600

    feat: settings screen

--- CreateWithCode/OneButtonGame/Assets/Scripts/Game Management/Settings/ButtonPositionManager.cs
+++ CreateWithCode/OneButtonGame/Assets/Scripts/Game Management/Settings/ButtonPositionManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonPositionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject rightButton;
+    [SerializeField] GameObject leftButton;
+    [SerializeField] TMP_Dropdown dropdown;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
     void Start()
     {
-        
+        // Add event listener for when the dropdown changes
+        dropdown.onValueChanged.AddListener(delegate
+        {
+            OnButtonLocationChange(dropdown);
+        });
+    }
+    public enum ButtonLocations
+    {
+        RIGHT,
+        LEFT
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButtonLocationChange(TMP_Dropdown change)
     {
+        Debug.Log("Dropdown changed!");
         
+        rightButton.SetActive(false);
+        leftButton.SetActive(false);
+
+        if (change.value == (int)ButtonLocations.RIGHT)
+        {
+            rightButton.SetActive(true);
+        }
+        else
+        {
+            leftButton.SetActive(true);
+        }
     }
 }
