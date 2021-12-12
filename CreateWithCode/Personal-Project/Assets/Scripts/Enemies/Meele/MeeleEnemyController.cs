37475160254377df5e71d353d6a146055106a828
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-12-11 20:09:28 -0600

    feat: menus and enemy audio

--- CreateWithCode/Personal-Project/Assets/Scripts/Enemies/Meele/MeeleEnemyController.cs
+++ CreateWithCode/Personal-Project/Assets/Scripts/Enemies/Meele/MeeleEnemyController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class MeeleEnemyController : EnemyTarget
 {
     [SerializeField] float speed;
     Transform player;
     GameManager gameManager;
+    AudioSource enemyAudio;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -17,6 +19,7 @@ public class MeeleEnemyController : EnemyTarget
         base.Start();
         player = GameObject.Find("Player").GetComponent<Transform>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        enemyAudio = GetComponent<AudioSource>();
     }
 
     /// <summary>
@@ -24,11 +27,26 @@ public class MeeleEnemyController : EnemyTarget
     /// </summary>
     void Update()
     {
-        if (gameManager.IsRunning && gameManager.PlayerStartedMaze)
+        if (gameManager.IsRunning)
         {
-            Vector3 path = player.position - transform.position;
-            path = new Vector3(path.x, 0, path.z);
-            transform.Translate(speed * Time.deltaTime * path.normalized);
+            if (gameManager.PlayerStartedMaze)
+            {
+                Vector3 path = player.position - transform.position;
+                path = new Vector3(path.x, 0, path.z);
+                transform.Translate(speed * Time.deltaTime * path.normalized);
+            }
+
+            if (!enemyAudio.isPlaying)
+            {
+                enemyAudio.Play();
+            }
+        }
+        else
+        {
+            if (enemyAudio.isPlaying)
+            {
+                enemyAudio.Stop();
+            }
         }
     }
 }
